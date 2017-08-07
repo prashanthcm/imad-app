d65badff74e716b485691672ba7b75432cab2a0b
prashanthcm <30742332+prashanthcm@users.noreply.github.com>
2017-08-07 19:51:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-alert("i am ur webapp");
-
+app.get('article-one', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
