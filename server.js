2844d867a44cde1abcc8d411a66a4926414378af
prashanthcm <30742332+prashanthcm@users.noreply.github.com>
2017-08-08 12:29:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.get('/ui/style.css', function (req, res) {
 var counter = 0;
 app.get('/counter', function (req, res) {
  counter = counter+1;
- res.send(counter.tostring());
+ res.send(counter.toString());
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
