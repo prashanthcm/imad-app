8db1f1c775216bea5440a0a76c262a4be7f9f0d4
prashanthcm <30742332+prashanthcm@users.noreply.github.com>
2017-10-16 15:24:19 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,6 +14,9 @@ app.get('/article-one', function (req, res) {
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
+app.get('/wordcount', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'wordcount.html'));
+});
 var counter = 0;
 app.get('/counter', function (req, res) {
  counter = counter+1;
